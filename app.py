6c134765d45e98f6713ded1bb26cef826f02a5a0
senseiAbhi <156998912+senseiAbhinav@users.noreply.github.com>
2025-07-28 10:22:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
+
+@app.route('/')
+def index():
+    tasks = Task.query.order_by(Task.id).all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task_content = request.form['content']
+    if task_content:
+        new_task = Task(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:id>')
+def complete(id):
+    task = Task.query.get_or_404(id)
+    task.completed = not task.completed
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    if not os.path.exists('todo.db'):
+        with app.app_context():
+            db.create_all()
+    app.run(debug=True)
